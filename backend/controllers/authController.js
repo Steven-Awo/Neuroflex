53884beaaae0429b32d82af01c6501ed5dc507d8
Rotimi Olakunle O. Awomodu <83477902+Steven-Awo@users.noreply.github.com>
2025-03-18 12:46:04 +0100

    back end
    sign up and signin works on postman but the api routes are yet to be protected

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,17 +1,17 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const generateToken = require("../config/jwt");
 
 // User Signup
 exports.signup = async (req, res) => {
-    const { name, email, password, role } = req.body;
+    const { name, email, password } = req.body;
 
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(400).json({ message: "User already exists" });
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ name, email, password: hashedPassword, role });
+        const user = new User({ name, email, password: hashedPassword });
         await user.save();
 
         res.json({ token: generateToken(user), user });
@@ -35,4 +35,13 @@ exports.login = async (req, res) => {
     } catch (err) {
         res.status(500).json({ message: "Server Error" });
     }
+};
+
+// Generate JWT Token
+const generateToken = (user) => {
+    return jwt.sign(
+        { id: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+    );
 };
